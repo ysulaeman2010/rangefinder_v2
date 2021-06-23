ae192e1bafc32d725b8b1f45fcd431c16f467063
prajadimas <dmspraja2105@gmail.com>
2021-06-23 09:53:56 +0700

    add get by id

--- server/src/controllers/config.js
+++ server/src/controllers/config.js
@@ -1,17 +1,21 @@
 const db = require("nedb");
+const { customAlphabet } = require('nanoid');
 const fun = require("../utils/functions");
+const nanoid = customAlphabet('0123456789abcdefghijklmnopqrstuvwxyz', 16);
+
 
 const database = new db("database.db");
 database.loadDatabase();
 
 exports.postData = (req, res, next) => {
-  const id = req.body.id;
+  const name = req.body.id;
   const port = req.body.port;
   const baudrate = req.body.baudrate;
 
   const result = {
     timestamp: fun.time(),
-    id: id,
+    _id: nanoid(),
+    name: name,
     port: port,
     baudrate: baudrate,
   };
@@ -22,9 +26,9 @@ exports.postData = (req, res, next) => {
   res.status(201).json(result);
   next();
 };
-
+// darvm6u0whxwmpc7
 exports.getData = (req, res, next) => {
-  database.find({}, (err, data) => {
+  database.find({ _id: req.params.id }, (err, data) => {
     if (err) {
       const error = {
         errMessage: err,
--- server/src/routes/config.js
+++ server/src/routes/config.js
@@ -4,6 +4,6 @@ const configController = require("../controllers/config");
 const router = express.Router();
 
 router.post("/config/post/", configController.postData);
-router.get("/config/get/", configController.getData);
+router.get("/config/get/:id", configController.getData);
 
 module.exports = router;
